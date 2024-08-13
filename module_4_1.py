b2c913fb88cacf84f93fc9fa549b6e722800663b
IronPhilosopher <boinchan.t@gmail.com>
2024-08-13 20:54:16 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from true_math import divide as tr_div
+from fake_math import divide as sc_div
+result1 = tr_div(1, 8)
+result2 = sc_div(5, 0)
+result3 = tr_div(27, 0)
+result4 = sc_div(44, 16)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
