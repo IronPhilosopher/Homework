4572d7ee99e8bc115187b040fd929737baf0a2e8
IronPhilosopher <boinchan.t@gmail.com>
2024-09-16 20:03:14 +0300

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -1,19 +1,24 @@
 class Horse:
-    x_distance = 0
-    sound = 'Frrr'
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
+        super().__init__()
+
     def run(self, dx):
         self.x_distance += dx
 
 class Eagle:
-    y_distance = 0
-    sound = "I train, eat, sleep, and repeat"
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = "I train, eat, sleep, and repeat"
+
     def fly(self, dy):
         self.y_distance += dy
 
 class Pegasus(Horse, Eagle):
     def __init__(self):
-        self.x_distance = 0
-        self.y_distance = 0
+        super().__init__()
+        super().__init__()
 
     def move(self, dx, dy):
         super().run(dx)
@@ -23,7 +28,7 @@ class Pegasus(Horse, Eagle):
         return (self.x_distance, self.y_distance)
 
     def voice(self):
-        print(super().sound)
+        print(self.sound)
 
 Peg = Pegasus()
 
@@ -34,4 +39,3 @@ Peg.move(-5, 20)
 print(Peg.get_pos())
 
 Peg.voice()
-# Пегас издает 'Frrr', т.к. по наследованию сначала Horse. В условии/решении задачи ошибка.
