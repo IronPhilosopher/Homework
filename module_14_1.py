fd47bce3b4fe32decb650be369724963056d1da9
IronPhilosopher <boinchan.t@gmail.com>
2024-12-01 15:16:30 +0300

    Add files via upload

---
+++ module_14_1.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+for i in range(10):
+    cursor.execute(' INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                   (f'User{i+1}', f'examlpe{i+1}@gmail.com', f'{(i+1)*10}', '1000'))
+
+cursor.execute('UPDATE Users SET balance = ? WHERE age % 20 != 0', (500,))
+cursor.execute('DELETE FROM Users WHERE (age-10) % 3 == 0')
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
+users = cursor.fetchall()
+for user in users:
+    print(user)
