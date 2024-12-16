57b9f23b0383582927f436c8cdee097072b8332f
IronPhilosopher <boinchan.t@gmail.com>
2024-12-16 21:08:09 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -1,4 +1,5 @@
 import sqlite3
+import asyncio
 
 connection = sqlite3.connect('medbot.db')
 cursor = connection.cursor()
@@ -11,6 +12,16 @@ price INTEGER NOT NULL
 );            
 ''')
 
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER NOT NULL,
+balance INTEGER NOT NULL
+)
+''')
+
 def get_all_products():
     connection = sqlite3.connect('medbot.db')
     cursor = connection.cursor()
@@ -18,6 +29,27 @@ def get_all_products():
     products = cursor.fetchall()
     connection.close()
     return products
+
+
+def add_user(username:str, email:str, age:int):
+    connection = sqlite3.connect('medbot.db')
+    cursor = connection.cursor()
+    exist = is_included(username)
+    if not exist:
+        cursor.execute(' INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                       (username, email, age, 1000))
+        connection.commit()
+        connection.close()
+        return
+
+
+def is_included(username:str):
+    connection = sqlite3.connect('medbot.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT username FROM Users WHERE username = ?', (username,))
+    t = cursor.fetchone()
+    connection.close()
+    return t is not None
 
 
 connection.commit()
