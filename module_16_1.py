02bb26c192510ec33b43cda907afb76feb39225a
IronPhilosopher <boinchan.t@gmail.com>
2025-01-16 20:26:17 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -4,16 +4,17 @@ app1 = FastAPI()
 
 @app1.get('/')
 async def main_page():
-    return {"Главная страница"}
+    return "Главная страница"
 
 @app1.get('/user/admin')
 async def admin_page():
-    return {"Вы вошли как администратор"}
+    return "Вы вошли как администратор"
 
 @app1.get('/user/{user_id}')
 async def user_page(user_id: int):
-    return {f"Вы вошли как пользователь № {user_id}"}
+    return f"Вы вошли как пользователь № {user_id}"
 
 @app1.get('/user')
 async def user_info(username: str = 'user', age: int = 22):
-    return {f"Информация о пользователе. Имя: {username}, Возраст: {age}."}
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}."
+    
