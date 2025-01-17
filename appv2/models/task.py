fd788505461ef6b91830ebf8e33901a4138782d4
IronPhilosopher <boinchan.t@gmail.com>
2025-01-17 23:34:45 +0300

    Add files via upload

---
+++ appv2/models/task.py
@@ -0,0 +1,16 @@
+from testapp.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False, index=True)
+    slug = Column(String, unique=True, index=True)
+    user = relationship('Task', back_populates='tasks')
