2e7ad598feac551d7607da0f7a6bf657803c6ef9
IronPhilosopher <boinchan.t@gmail.com>
2025-01-19 21:48:16 +0300

    Add files via upload

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from task3.views import main_page, suits, future
+from task4.views import main_page, suits, future
 
 urlpatterns = [
     path('admin/', admin.site.urls),
---
+++ UrbanDjango/task4/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+
+
+def main_page(request):
+    return render(request, 'main_page.html')
+
+
+def suits(request):
+    suit1 = 'Мечи'
+    suit2 = 'Жезлы'
+    suit3 = 'Кубки'
+    suit4 = 'Монеты'
+    context = {
+        't_suits': [suit1, suit2, suit3, suit4],
+    }
+    return render(request, 'suits.html', context)
+
+
+def future(request):
+    return render(request, 'future.html')
